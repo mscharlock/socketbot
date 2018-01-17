3552eb64c0779de40502d211e8dcc85a15869fb9
mscharlock <michelle.scharlock@gmail.com>
2018-01-17 11:47:24 -0800

    Modifying with another tut but infinite loop of users connecting

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 //Requiring in what we need
-const app = require('express')();
-const http = require('http').Server(app);
+const express = require('express');
+const app = express();
+const http = require('http').Server(express);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
 
-//Route handler
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/view/index.html');
 });
 
-//chat turns on and transmits messages
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+io.on('connection', function(socket) {
+  console.log('user connected!');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
   });
 });
 
+//
+// const socket = io.connectWithSession();
+// prepareSocketIO();
+// socket.on('connect', function() {
+//   console.log('connection made');
+//   socketReady= true;
+// })
+
+
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
+// const port = process.env.PORT || 3000;
+
+//Route handler
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+
+//chat turns on and transmits messages
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     console.log('message: ' + msg);
+//   });
+// });
 
 
 //Console log to make sure server is up
