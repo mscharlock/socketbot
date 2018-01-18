41df1c95715c047e81a0c5c5bf714b303244333b
mscharlock <michelle.scharlock@gmail.com>
2018-01-17 18:10:44 -0800

    Trying another tut

--- index.js
+++ index.js
@@ -1,69 +1,102 @@
 //Requiring in what we need
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const express = require('express');
+// const app = express();
+// const http = require('http').Server(app);
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+//server related variables
+const port = 3000;
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+
+//chat related variables
+let history = [];
+let clients = [];
+
+
+//Helper function for escaping input strings
+function htmlEntities(str) {
+  return String(str)
+      .replace(/&/g, '&amp;').replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+//http server
+const server = http.createServer(function (req, res) {
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+server.listen(port, function() {
+  console.log('Server listening on: ' + port);
+});
+
+//connect the wsServer to http server
+const wsServer = new WebSocketServer({
+  httpServer: server,
+});
+
+//
+wsServer.on('request', function(request) {
+  console.log('Connection from origin: ' + request.origin);
+  let connection = request.accept(null, request.origin);
+
+  let index = clients.push(connection) - 1;
+  let userName = false;
+
+  console.log(('Connection accepted'));
+
+  if (history.length > 0) {
+    connection.sendUTF(
+      JSON.stringify({type: 'history', data: history})
+    );
+  }
+
+  connection.on('message', function(message) {
+    if(message.type === 'utf8') {
+    if (userName === false) {
+      userName = htmlEntities(message.utf8Data);
+      console.log('Username: ' + userName);
+    } else {
+      console.log('Received a message from :' + userName + ': ' + message.utf8Data);
+
+      let historyTracker = {
+        text: htmlEntities(message.utf8Data),
+        author: userName,
+      };
+      history.push(historyTracker);
+
+      let json = JSON.stringify({type: 'message', data: historyTracker});
+      for (let i = 0; i < clients.length; i++) {
+        clients[i].sendUTF(json);
+      }
+    }
+    }
+  });
+
+  connection.on('close', function(connection) {
+    if (userName !== false) {
+      console.log('Disconnected: ' + connection.remoteAddress);
+    clients.splice(index, 1);
+    }
   });
 });
 
- // Handle form submits
-// document.querySelector('form').addEventListener('submit', e => submitMessage(e, socket));
-//
 
 
-//Console log to make sure server is up
-http.listen(3000, function(){
-  console.log('listening on PORT:3000');
-});
-
-
-
-// io.on('connection', function(socket) {
-//
-//   socket.on('join', function(data) {
-//     socket.join(data.type);
-//     socket.session = {
-//       name: data.name,
-//     };
-//     console.log(data);
-//     console.log('someone joined');
-//   });
-// });
-//
-//
-// io.on('disconnect', function() {
-//   console.log('connection closed');
-// });
-// // const socket = io.connectWithSession();
-// prepareSocketIO();
-// socket.on('connect', function() {
-//   console.log('connection made');
-//   socketReady= true;
-// })
-
-
-// const http = require('http').Server(app);
 // const io = require('socket.io')(http);
-// const port = process.env.PORT || 3000;
-
-//Route handler
-// app.get('/', function(req, res){
+//
+//
+// app.get('/', function(req, res) {
 //   res.sendFile(__dirname + '/index.html');
 // });
-
-
-//chat turns on and transmits messages
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log('message: ' + msg);
+//
+// //Open the socket and emit//
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
 //   });
+// });
+//
+// //Console log to make sure server is up
+// http.listen(3000, function(){
+//   console.log('listening on PORT:3000');
 // });
